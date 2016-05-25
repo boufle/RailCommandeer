0ab4daa261bb515769ce891fd4f2d0bb621a1d6b
Boufle <169154@supinfo.com>
2016-05-25 15:15:30 +0200

    kiuhiuoh

--- app/src/main/java/fr/railcommandeer/app/Materials/SearchCard.java
+++ app/src/main/java/fr/railcommandeer/app/Materials/SearchCard.java
@@ -1,87 +1,145 @@
-package fr.railcommandeer.app.Materials;
 
-import android.content.Context;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-import fr.railcommandeer.app.R;
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.internal.base.BaseCard;
-import it.gmariotti.cardslib.library.prototypes.CardWithList;
+        package fr.railcommandeer.app.Materials;
 
-import java.util.ArrayList;
-import java.util.List;
+        import android.content.Context;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import com.squareup.picasso.Picasso;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+        import fr.railcommandeer.app.R;
+        import it.gmariotti.cardslib.library.internal.Card;
+        import it.gmariotti.cardslib.library.internal.CardHeader;
+        import it.gmariotti.cardslib.library.prototypes.CardWithList;
+        import it.gmariotti.cardslib.library.prototypes.LinearListView;
 
 /**
- * Created by Boufle on 19/05/2016.
+ * @author Gabriele Mariotti (gabri.mariotti@gmail.com)
  */
 public class SearchCard extends CardWithList {
 
     public SearchCard(Context context) {
         super(context);
-        SearchCard card = new SearchCard(context);
-        card.init();
     }
 
     @Override
     protected CardHeader initCardHeader() {
-        CardHeader header = new CardHeader(getContext());
 
-        //Add a popup menu. This method set OverFlow button to visible
-       /* header.setPopupMenu(R.menu.popup_item, new CardHeader.OnClickCardHeaderPopupMenuListener() {
+        //Add Header
+        CardHeader header = new CardHeader(getContext(), R.layout.searchcard_inner_header) {
+
             @Override
-            public void onMenuItemClick(BaseCard card, MenuItem item) {
-
-                switch (item.getItemId()){
-                    case R.id.action_add:
-                        WeatherObject w1= new WeatherObject(MyCard.this);
-                        w1.city ="Madrid";
-                        w1.temperature = 24;
-                        w1.weatherIcon = R.drawable.ic_action_sun;
-                        w1.setObjectId(w1.city);
-                        mLinearListAdapter.add(w1);
-                        break;
-                    case R.id.action_remove:
-                        mLinearListAdapter.remove(mLinearListAdapter.getItem(0));
-                        break;
+            public void setupInnerViewElements(ViewGroup parent, View view) {
+                super.setupInnerViewElements(parent, view);
+                TextView subTitle = (TextView) view.findViewById(R.id.carddemo_googleknow_sub_title);
+                if (subTitle != null) {
+                    subTitle.setText("To see posts from friends and family,add them.");  //Should use strings.xml
                 }
             }
-        });*/
-        header.setTitle("Weather"); //should use R.string.
+        };
+        header.setTitle("You may know"); //should use strings.xml
         return header;
     }
 
     @Override
     protected void initCard() {
 
-        setSwipeable(true);
-        setOnSwipeListener(new OnSwipeListener() {
-            @Override
-            public void onSwipe(Card card) {
-                Toast.makeText(getContext(), "Swipe on " + card.getCardHeader().getTitle(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
+
 
     @Override
     protected List<ListObject> initChildren() {
+
+        //Init the list
         List<ListObject> mObjects = new ArrayList<ListObject>();
 
-        TextViewCard w1= new TextViewCard(this.getContext());
-        mObjects.add(w1);
+        //Add an object to the list
+        MayKnowObject s1 = new MayKnowObject(this);
+        s1.name = "Gabriele Mariotti";
+        s1.common = "127 people in common";
+        s1.url = "https://lh5.googleusercontent.com/-squZd7FxR8Q/UyN5UrsfkqI/AAAAAAAAbAo/VoDHSYAhC_E/s54/new%2520profile%2520%25282%2529.jpg";
+        mObjects.add(s1);
+
+        //Add an object to the list
+        MayKnowObject s2 = new MayKnowObject(this);
+        s2.name = "Another user";
+        s2.common = "3 people in common";
+        s2.url = "https://lh5.googleusercontent.com/-squZd7FxR8Q/UyN5UrsfkqI/AAAAAAAAbAo/VoDHSYAhC_E/s54/new%2520profile%2520%25282%2529.jpg";
+        mObjects.add(s2);
 
         return mObjects;
     }
 
     @Override
-    public View setupChildView(int i, ListObject listObject, View view, ViewGroup viewGroup) {
-        return null;
+    public View setupChildView(int childPosition, ListObject object, View convertView, ViewGroup parent) {
+
+        //Setup the ui elements inside the item
+        TextView textViewName = (TextView) convertView.findViewById(R.id.carddemo_know_name);
+        TextView textViewPeople = (TextView) convertView.findViewById(R.id.carddemo_know_common);
+        ImageView imagePeople = (ImageView) convertView.findViewById(R.id.carddemo_know_image);
+
+
+        //Retrieve the values from the object
+        MayKnowObject stockObject = (MayKnowObject) object;
+        textViewName.setText(stockObject.name);
+        textViewPeople.setText(""+stockObject.common);
+
+        Picasso.with(getContext()).setIndicatorsEnabled(false);
+        Picasso.with(getContext()).load(stockObject.url)
+                .into(imagePeople);
+
+        return convertView;
     }
 
     @Override
     public int getChildLayoutId() {
-        return 0;
+        return R.layout.searchcard_inner_main;
     }
+
+
+    // -------------------------------------------------------------
+    // Weather Object
+    // -------------------------------------------------------------
+
+    public class MayKnowObject extends DefaultListObject {
+
+        public String name;
+        public String common;
+        public String url;
+
+        public MayKnowObject(Card parentCard) {
+            super(parentCard);
+            init();
+        }
+
+        private void init() {
+            //OnClick Listener
+            setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(LinearListView parent, View view, int position, ListObject object) {
+                    Toast.makeText(getContext(), "Click on " + getObjectId(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //OnItemSwipeListener
+            setOnItemSwipeListener(new OnItemSwipeListener() {
+                @Override
+                public void onItemSwipe(ListObject object, boolean dismissRight) {
+                    Toast.makeText(getContext(), "Swipe on " + object.getObjectId(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public String getObjectId() {
+            return name;
+        }
+    }
+
 }
--- app/src/main/java/fr/railcommandeer/app/SearchTrainActivity.java
+++ app/src/main/java/fr/railcommandeer/app/SearchTrainActivity.java
@@ -1,19 +1,26 @@
 package fr.railcommandeer.app;
 
+import android.app.DatePickerDialog;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
 
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.*;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialize.util.UIUtils;
+import fr.railcommandeer.app.Materials.SearchCard;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardViewNative;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SearchTrainActivity extends AppCompatActivity {
     private static final int PROFILE_SETTING = 1;
@@ -21,6 +28,17 @@ public class SearchTrainActivity extends AppCompatActivity {
     //save our header or result
     private AccountHeader headerResult = null;
     private Drawer result = null;
+    private DatePickerDialog DepartDatePickerDialog;
+    private DatePickerDialog ArriveDatePickerDialog;
+    private DatePickerDialog DepartDatePickerDialog2;
+    private DatePickerDialog ArriveDatePickerDialog2;
+    private SimpleDateFormat dateFormatter;
+    EditText editText;
+    EditText dateArriver;
+    EditText editText2;
+    EditText dateArriver2;
+    Switch aSwitch;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +47,21 @@ public class SearchTrainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_searchtrainactivity);
+        SearchCard searchCard = new SearchCard(getApplicationContext());
+        CardHeader header = new CardHeader(getApplicationContext());
+        searchCard.addCardHeader(header);
 
+        editText = (EditText) findViewById(R.id.date);
+        aSwitch = (Switch) findViewById(R.id.switch1);
+        relativeLayout = (RelativeLayout) findViewById(R.id.trajetRetour);
+        dateArriver = (EditText) findViewById(R.id.dateArrive);
+        editText2 = (EditText) findViewById(R.id.date2);
+        dateArriver2 = (EditText) findViewById(R.id.dateArrive2);
 
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        //CardViewNative cardView = (CardViewNative) findViewById(R.id.carddemo);
+        //cardView.setCard(searchCard);
+        setDateTimeField();
         // Handle Toolbar
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -39,6 +70,19 @@ public class SearchTrainActivity extends AppCompatActivity {
         //set the back arrow in the toolbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(false);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+                if (isChecked){
+                    relativeLayout.setVisibility(View.VISIBLE);
+                }else
+                {
+                    relativeLayout.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
@@ -49,6 +93,55 @@ public class SearchTrainActivity extends AppCompatActivity {
         outState = headerResult.saveInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
+
+
+
+    private void setDateTimeField() {
+
+        Calendar newCalendar = Calendar.getInstance();
+        DepartDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                editText.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+
+        ArriveDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                dateArriver.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        DepartDatePickerDialog2 = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                editText2.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+
+        ArriveDatePickerDialog2 = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                dateArriver2.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -71,6 +164,22 @@ public class SearchTrainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    public void onClickDepartDate(View view) {
+        DepartDatePickerDialog.show();
+    }
+
+    public void onClickArriveDate(View view) {
+        ArriveDatePickerDialog.show();
+    }
+
+    public void onClickDepartDate2(View view) {
+        DepartDatePickerDialog2.show();
+    }
+
+    public void onClickArriveDate2(View view) {
+        ArriveDatePickerDialog2.show();
     }
 
     class ActionBarCallBack implements ActionMode.Callback {
@@ -101,5 +210,6 @@ public class SearchTrainActivity extends AppCompatActivity {
         public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
             return false;
         }
+
     }
 }
